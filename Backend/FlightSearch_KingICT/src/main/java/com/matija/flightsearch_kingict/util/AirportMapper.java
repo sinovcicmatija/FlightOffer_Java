3a67e9b7656d839ecaf70ea98337a9b215b194af
sinovcicmatija <163353550+sinovcicmatija@users.noreply.github.com>
2025-04-14 15:45:56 +0200

    Fixed small mistake in AirportMapper

--- Backend/FlightSearch_KingICT/src/main/java/com/matija/flightsearch_kingict/util/AirportMapper.java
+++ Backend/FlightSearch_KingICT/src/main/java/com/matija/flightsearch_kingict/util/AirportMapper.java
@@ -7,7 +7,7 @@ public class AirportMapper {
 
     public AirportDTO toDTO(LocationResponse locationResponse) {
         AirportDTO dto = new AirportDTO();
-        dto.setAirportName(locationResponse.getLocationData().getAddress().getCityName());
+        dto.setCityName(locationResponse.getLocationData().getAddress().getCityName());
         dto.setAirportName(locationResponse.getLocationData().getName());
         dto.setIataCode(locationResponse.getLocationData().getIataCode());
         dto.setCountryCode(locationResponse.getLocationData().getAddress().getCountryCode());
