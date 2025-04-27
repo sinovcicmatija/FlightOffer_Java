80dda4356b7f1462ea998bc53afe80724e0cee98
sinovcicmatija <sinovcicmatija@gmail.com>
2025-04-27 14:39:02 +0200

    Added docker-compose

--- Backend/FlightSearch_KingICT/src/main/java/com/matija/flightsearch_kingict/config/CorsConfig.java
+++ Backend/FlightSearch_KingICT/src/main/java/com/matija/flightsearch_kingict/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React
+                        .allowedOrigins("http://localhost:3000", "http://frontend:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
